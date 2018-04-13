159a6e2d3f4354dab4a4176a6fa5001765ef5c37
Evan Zarowitz <ezarowitz@itemizecorp.com>
2018-04-13 16:38:24 -0400

    fixed prop name on loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,7 @@ import './Loader.css'
 import React from 'react';
 
 const Loader = () => (
-  <div class="animate-pulse">Loading...</div>
+  <div className="animate-pulse">Loading...</div>
 );
 
 export default Loader;
